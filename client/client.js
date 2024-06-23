f854784a71a30e694e03193a22cdc756fe2d3a5e
MOHD NEHAL KHAN <nehalk482@gmail.com>
2024-06-23 16:22:59 +0530

    Update the code to view wind,sunset,sunrise data on Web app

--- client/client.js
+++ client/client.js
@@ -16,11 +16,16 @@ searchBtn.addEventListener('click', async () => {
 function updateWeatherInfo(data) {
   try {
     const current = data.current;
+    const forecast = data.forecast;
     document.querySelector('.cityName').innerText = current.location.name;
     document.querySelector('.cityTemp').innerText = `${current.current.temp_c}°C`;
     document.querySelector('.cityCond').innerText = current.current.condition.text;
     document.querySelector('.date').innerText = new Date(current.current.last_updated).toLocaleDateString();
     document.querySelector('.aqi').innerText = `${current.current.air_quality.pm10} AQI`;
+    document.querySelector('.humid').innerText = `${current.current.humidity}%`;
+    document.getElementById('wind').innerText = `${current.current.wind_kph} km/h`;
+    document.querySelector('.Rise').innerText = forecast.forecast.forecastday[0].astro.sunrise;
+    document.querySelector('.Set').innerText = forecast.forecast.forecastday[0].astro.sunset;
   } catch (error) {
     console.error('Error updating weather info:', error);
     // Optionally, update the DOM to show an error message or clear outdated information
