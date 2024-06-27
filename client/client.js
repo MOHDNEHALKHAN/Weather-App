bdb3ca1fc4e38203c6988a88b9d499d89a5172c4
MOHD NEHAL KHAN <nehalk482@gmail.com>
2024-06-27 17:32:26 +0530

    Feature added : Weather Icn change as per weather condition

--- client/client.js
+++ client/client.js
@@ -5,6 +5,7 @@ searchBtn.addEventListener('click', async () => {
   try {
       const response = await fetch(`/weather/${location}`);
       const weatherData = await response.json();
+      console.log(weatherData);
       // Call your function to update the DOM with weatherData
       updateWeatherInfo(weatherData);
   } catch (error) {
@@ -16,6 +17,7 @@ function updateWeatherInfo(data) {
   try {
     const current = data.current;
     const forecast = data.forecast;
+    const weatherIcon = document.querySelector('.weatherImg');
     document.querySelector('.cityName').innerText = current.location.name;
     document.querySelector('.cityTemp').innerText = `${current.current.temp_c}°C`;
     document.querySelector('.cityCond').innerText = current.current.condition.text;
@@ -28,8 +30,52 @@ function updateWeatherInfo(data) {
     document.querySelector('.morn').innerText = `${forecast.forecast.forecastday[0].hour[7].temp_c}°C`;
     document.querySelector('.even').innerText = `${forecast.forecast.forecastday[0].hour[18].temp_c}°C`;
     document.querySelector('.night').innerText = `${forecast.forecast.forecastday[0].hour[23].temp_c}°C`;
+
+    if(current.current.condition.text === 'Sunny'){
+      weatherIcon.src = 'Assets/WeatherImg/sunny.png';
+    }
+    else if(current.current.condition.text === 'Partly cloudy'){
+      weatherIcon.src = 'Assets/WeatherImg/partlycloudy.png';
+    }
+    else if(current.current.condition.text === 'Cloudy'){
+      weatherIcon.src = 'Assets/WeatherImg/cloudy.png';
+    }
+    else if(current.current.condition.text === 'Mist'){
+      weatherIcon.src = 'Assets/WeatherImg/drizzle.png';
+    }
+    else if(current.current.condition.text === 'Patchy rain possible'){
+      weatherIcon.src = 'Assets/WeatherImg/showers.png';
+    }
+    else if(current.current.condition.text === 'Light rain shower'){
+      weatherIcon.src = 'Assets/WeatherImg/showers.png';
+    }
+    else if(current.current.condition.text === 'Fog'){
+      weatherIcon.src = 'Assets/WeatherImg/partlycloudy.png';
+    }
+    else if(current.current.condition.text === 'Light drizzle'){
+      weatherIcon.src = 'Assets/WeatherImg/drizzle.png';
+    }
+    else if(current.current.condition.text === 'Heavy rain'){
+      weatherIcon.src = 'Assets/WeatherImg/showers.png';
+    }
+    else if(current.current.condition.text === 'Partly cloudy'){
+      weatherIcon.src = 'Assets/WeatherImg/hail.png';
+    }
+    else if(current.current.condition.text === 'Heavy snow'){
+      weatherIcon.src = 'Assets/WeatherImg/snow.png';
+    }
+    else if(current.current.condition.text === 'Patchy light rain with thunder'){
+      weatherIcon.src = 'Assets/WeatherImg/isolatedthunderstorms.png';
+    }
+    else if(current.current.condition.text === 'Moderate or heavy rain with thunder'){
+      weatherIcon.src = 'Assets/WeatherImg/thunderstorms.png';
+    }
+    else{
+      weatherIcon.src = 'Assets/WeatherImg/sunny.png';
+    }
   } catch (error) {
     console.error('Error updating weather info:', error);
     // Optionally, update the DOM to show an error message or clear outdated information
   }
+  
 }
